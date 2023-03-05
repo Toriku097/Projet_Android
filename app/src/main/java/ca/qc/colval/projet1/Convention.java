89df566efc3f342afe16208e0860f86a97c13467
Toriku097 <ttramirez097@gmail.com>
2023-03-05 15:23:24 -0500

    Initial

---
+++ app/src/main/java/ca/qc/colval/projet1/Convention.java
@@ -0,0 +1,14 @@
+package ca.qc.colval.projet1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Convention extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convention);
+    }
+}
