b97e7880bc9829930e938c05338f1e47a156cab4
Toriku097 <ttramirez097@gmail.com>
2023-03-10 21:46:40 -0500

    début ConventionActivity avec initiation

--- app/src/main/java/ca/qc/colval/projet1/activity/ConventionActivity.java
+++ app/src/main/java/ca/qc/colval/projet1/activity/ConventionActivity.java
@@ -3,14 +3,24 @@ package ca.qc.colval.projet1.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import ca.qc.colval.projet1.R;
 
 public class ConventionActivity extends AppCompatActivity {
 
+    TextView txt_supplier;
+    Spinner spn_supplier,spn_convention;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convention);
+
+        //init
+        txt_supplier = findViewById(R.id.convention_txt_supplier);
+        spn_supplier = findViewById(R.id.convention_spn_supplier);
+        spn_convention = findViewById(R.id.convention_spn_convention);
     }
 }
