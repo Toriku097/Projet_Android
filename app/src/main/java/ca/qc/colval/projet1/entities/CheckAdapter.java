858657f94beb1b22b1e5fc5d1189c02e49b49535
bruh man <Darian321@outlook.com>
2023-03-13 20:28:30 -0400

    DeadlineActivity work now (no lmao)

--- app/src/main/java/ca/qc/colval/projet1/entities/CheckAdapter.java
+++ app/src/main/java/ca/qc/colval/projet1/entities/CheckAdapter.java
@@ -18,9 +18,6 @@ public class CheckAdapter extends RecyclerView.Adapter<CheckViewHolder> {
 
     List<Check> checks;
 
-    BankAccountDAO bankDAO;
-    ProjectDAO projectDAO;
-
     public CheckAdapter(List<Check> checks) {
         this.checks = checks;
     }
@@ -35,10 +32,8 @@ public class CheckAdapter extends RecyclerView.Adapter<CheckViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull CheckViewHolder holder, int position) {
         holder.checkNum.setText(checks.get(position).getCheckNum());
-        holder.account.setText(bankDAO.getBankAccountNobyId(checks.get(position).getAccountId()));
         holder.amount.setText(String.valueOf(checks.get(position).getAmount()));
         holder.date.setText(checks.get(position).getDeadlineDate());
-        holder.project.setText(projectDAO.getProjectNamebyId(checks.get(position).getProjectId()));
     }
 
     @Override
